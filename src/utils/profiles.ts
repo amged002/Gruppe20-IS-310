135df03dd06571c05af68b8620c7d1dca5aaea3b
Amgad <amged4485@gmail.com>
2026-09-02 22:10:36 +0100

    Fix profile image path

--- src/utils/profiles.ts
+++ src/utils/profiles.ts
@@ -18,7 +18,7 @@ export const profiles: Profile[] = [
         'Gjennom studiet har jeg fått erfaring med blant annet programmering, webutvikling, databaser og utvikling av informasjonssystemer. Jeg liker å lære nye teknologier og trives godt med praktisk problemløsning og samarbeid med andre.\n' +
         '\n' +
         'Jeg har også erfaring fra sikkerhet, kundeservice og praktisk arbeid, noe som har gitt meg gode ferdigheter innen kommunikasjon, ansvar og samarbeid.\n',
-    image: 'src/assets/images/Amged.jpeg',
+    image: '/Amged.jpeg',
     github: 'https://github.com/amged002',
     linkedin: 'https://www.linkedin.com/in/amgad-mohamed-59811525b/',
   },
