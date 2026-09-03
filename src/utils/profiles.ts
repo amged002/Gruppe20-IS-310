7239b1d30b2fe1ee2cc83b8a6ba3235f0180197c
Amgad <amged4485@gmail.com>
2026-09-03 19:01:34 +0100

    la inn storm

--- src/utils/profiles.ts
+++ src/utils/profiles.ts
@@ -11,20 +11,25 @@ export const profiles: Profile[] = [
   {
     name: 'Amgad Mohamed',
     role: 'Rolle / Tittel',
-    description:
-        'Jeg heter Amgad Mohamed og studerer IT og informasjonssystemer ved Universitetet i Agder. Jeg er spesielt interessert i programmering, systemutvikling og hvordan teknologi kan brukes til å utvikle praktiske og brukervennlige løsninger.\n\n' +
-        'Gjennom studiet har jeg fått erfaring med programmering, webutvikling, databaser og utvikling av informasjonssystemer. Jeg liker å lære nye teknologier og trives godt med problemløsning og samarbeid med andre.\n\n' +
-        'Jeg har også erfaring fra sikkerhet, kundeservice og praktisk arbeid, noe som har gitt meg gode ferdigheter innen kommunikasjon, ansvar og samarbeid.',
-     image: '/Amged.jpeg',
+    description:'Jeg heter Amgad Mohamed og studerer IT og informasjonssystemer ved Universitetet i Agder. Jeg er spesielt interessert i programmering, systemutvikling og hvordan teknologi kan brukes til å utvikle praktiske og brukervennlige løsninger.\n' +
+        '\n' +
+        'Gjennom studiet har jeg fått erfaring med programmering, webutvikling, databaser og utvikling av informasjonssystemer. Jeg liker å lære nye teknologier og trives godt med problemløsning og samarbeid med andre.\n' +
+        '\n' +
+        'Jeg har også erfaring fra sikkerhet, kundeservice og praktisk arbeid. Dette har gitt meg gode ferdigheter innen kommunikasjon, ansvar og samarbeid, som jeg ønsker å ta med meg videre i IT-prosjekter.',
+    image: '/Amged.jpeg',
     github: 'https://github.com/amged002',
     linkedin: 'https://www.linkedin.com/in/amgad-mohamed-59811525b/',
   },
   {
-    name: 'Medlem 2',
+    name: 'Storm Haukom',
     role: 'Rolle / Tittel',
-    description: 'Legg inn din beskrivelse her. Fortell litt om deg selv, dine ferdigheter og interesser.',
-    image: '',
-    github: '',
+    description: 'Jeg er 22 år gammel, født og oppvokst i Kongsberg. Jeg studerer IT og informasjonssystemer ved UiA, samtidig som jeg jobber deltid som Technical Assistant hos Kongsberg Maritime.\n' +
+        '\n' +
+        'Hos Kongsberg Maritime jobber jeg i avdelingen for Dynamic Positioning (DP), hvor jeg blant annet arbeider med oppsett og konfigurering av datamaskiner, feilsøking, testing og problemløsing. Gjennom jobben får jeg praktisk erfaring med IT i et teknisk og maritimt miljø, og innblikk i hvordan komplekse teknologiske systemer utvikles, testes og vedlikeholdes.\n' +
+        '\n' +
+        'På fritiden har jeg stor interesse for kunstig intelligens, gaming, musikk og teknologi generelt. Jeg er spesielt interessert i hvordan ny teknologi kan brukes til å løse problemer, effektivisere arbeidsprosesser og skape nye muligheter.',
+    image: '/Storm.jpeg',
+    github: 'https://github.com/Storm300',
     linkedin: '',
   },
   {
